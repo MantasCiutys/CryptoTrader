603631c122f4e671e73576eebb4f971639cea621
MCiutys <ciutysmant@gmail.com>
2021-10-14 20:09:02 +0100

    First commit

---
+++ cryptotrader/src/test/java/com/mantasciutys/cryptotrader/CryptotraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mantasciutys.cryptotrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptotraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
